138b3df0c0559494daa029e6f0a914bcbebe07a0
Mohamed Walied <muhmedwalieed@gmail.com>
2025-09-02 14:35:33 +0300

    Update sendResponse to determine success by statusCode and implement password reset workflow

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoutes.js";
 import profileRoutes from "./routes/profileRoutes.js";
+import passwordRoutes from "./routes/passwordRoutes.js";
 import routineRoutes from "./routes/weeklyRoutineRoutes.js";
 import { errorHandler } from './utils/errorHandler.js';
 import connectDB from "./config/db.js";
@@ -18,6 +19,7 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/profile", profileRoutes);
+app.use("/api/password", passwordRoutes);
 app.use("/api/routines", routineRoutes);
 
 app.use(errorHandler);
